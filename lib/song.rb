68f4acaf072b0c6457dd2207b77a86575d6cb120
Keith Filkins <kafilkins@me.com>
2020-12-12 22:26:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,8 +29,8 @@ class Song
     def self.genre_count
       genre_count = {}
       @@genres.each do |genre|
-        if genre_count[genre] += 1
-          genre_count[genre]
+        if genre_count[genre]
+          genre_count[genre] +=1
         else
           genre_count[genre] = 1
         end
