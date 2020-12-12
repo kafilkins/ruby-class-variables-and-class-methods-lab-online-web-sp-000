b466790e52fa7ff7859e9d878f49b7a0b3f396be
Keith Filkins <kafilkins@me.com>
2020-12-12 21:58:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,5 +38,17 @@ attr_accessor :name, :artist, :genre
       genre_count
     end
 
+    def self.artist_count
+      artist_count = {}
+      @@artist.each do |artist|
+        if artist_count[artist]
+          artist_count += 1
+        else
+          artist_count[arist] = 1
+        end
+      end
+      artist_count
+    end
+
 
 end
