022d86b8833faf947525557a73fce99b80da9698
Keith Filkins <kafilkins@me.com>
2020-12-12 22:23:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,12 +18,12 @@ class Song
     @@count
     end
 
-    def self.genres
-    @@genres.uniq!
-    end
-
     def self.artists
     @@artists.uniq!
+    end
+
+    def self.genres
+    @@genres.uniq!
     end
 
     def self.genre_count
