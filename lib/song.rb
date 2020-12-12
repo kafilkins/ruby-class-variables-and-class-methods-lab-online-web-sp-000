b379944cf54deef5e7e44644d6629c0f9fcb14c2
Keith Filkins <kafilkins@me.com>
2020-12-12 22:17:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,7 @@ class Song
         if artist_count[artist]
           artist_count[artist] += 1
         else
-          artist_count[arist] = 1
+          artist_count[artist] = 1
         end
       end
       artist_count
