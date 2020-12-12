d193d6087d83549127cf9fb60e2a7eedd023322c
Keith Filkins <kafilkins@me.com>
2020-12-12 21:41:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,26 @@
 class Song
   @@count = 0
+  @@genre = 0
+  @@artist = []
 
 
   def initialize(name, artist, genre)
     @@count += 1
+    @@genre +=1
+    @@artist +=1
   end
 
   def self.count
     @@count
   end
 
+  def self.genre
+    new_array 
+  end
 
-
+    def self.artist
+      new_array
+    end
+      
 
 end
